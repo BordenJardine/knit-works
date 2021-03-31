88addfa7e80270b458bc33a74a2e8d11a5701edf
oliviaruizknott <olivia@ruiz-knott.com>
2021-03-31 07:40:01 -0600

    I hacked this to do what we ultimately want

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,3 +1,6 @@
+let id = null
+let currentId = null
+
 function load() {
   console.log('hello world')
   button = document.querySelector('button')
@@ -8,17 +11,35 @@ function load() {
 function startWebSockets() {
   const ws = new WebSocket("ws://" + window.location.host)
   ws.onmessage = onMessage
+  id = Date.now();
   return ws
 }
 
 function onMessage(e) {
-  console.debug("WebSocket message received:", e)
+  if (parseInt(e.data)) {
+    currentId = e.data
+    console.log(currentId)
+    disableIfCurrent()
+  } else {
+    console.log("Skipped")
+  }
+
+  // console.debug("WebSocket message received:", e)
 }
 
 function onClick(ws) {
   console.log('sending socket message')
-  ws.send('clicked!')
+  // ws.send('clicked!')
+  ws.send(id)
+}
+
+function disableIfCurrent() {
+  button = document.querySelector('button')
+  if (id == currentId) {
+    button.disabled = true
+  } else {
+    button.disabled = false
+  }
 }
 
 window.addEventListener('load', load)
-
